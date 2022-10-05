09065d7c27154efd2f1d8903d9aca3b066509d95
Halqq <kauahmsilva@gmail.com>
2022-10-04 22:13:00 -0300

    UiCryptor 0.0.6
    added base64 encoder
    fixed brain issue

--- src/dev/halq/Main.java
+++ src/dev/halq/Main.java
@@ -16,7 +16,7 @@ public class Main {
     public static String version = "0.0.6";
 
     public static void main(String[] args) throws IOException {
-        //UiMain.main();
+        UiMain.main();
         System.out.println("[UiCryptor] " + "Welcome to UiCryptor! crypto tool for files with algorithms AES, DES, RC2, BLOWFISH, Base64 encryption!");
         System.out.println("[UiCryptor] " + "Made by Halq");
         System.out.println("[UiCryptor] AES " + "Use Encrypt 216-key");
